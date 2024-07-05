0b49c70123e93187338525e987af9dff90e8146f
AquinoMarisol <aquinomarisol635@gmail.com>
2024-07-05 17:15:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-const btnlLike1 = document.getElementById("btnLike1") const countLike1 = document.getElementById("countLike1") function clickLike1(){ let totalLikès = countLike1.value + 1 countLike1.textContent = totalLikes.toString() } btnLike1.addventListener("click",clickLike1)
+
